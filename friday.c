a481ff998f424c89ef189e3d3446d602d9b675d6
Creature <785862780@qq.com>
2018-03-05 20:59:23 +0800

    Update friday.c

--- friday.c
+++ friday.c
@@ -10,11 +10,11 @@ int leap(int x){
 	return (x%4==0&&x%100!=0)||x%400==0?1:0;
 }
 int main(){
-	FILE *fin  = fopen ("friday.in", "r");
+    FILE *fin  = fopen ("friday.in", "r");
     FILE *fout = fopen ("friday.out", "w");
-	int N,i,j,last=1;
-	fscanf(fin,"%d",&N);
-   for(i=0;i<N;++i){
+    int N,i,j,last=1;
+    fscanf(fin,"%d",&N);
+    for(i=0;i<N;++i){
         for(j=1;j<=12;++j){
             last+=12;
             week[last%7]++;
@@ -22,6 +22,6 @@ int main(){
             else last+=month[j]-12;
         }
     }
-    printf("%d %d %d %d %d %d %d\n",week[6],week[0],week[1],week[2],week[3],week[4],week[5]);
+    fprintf(fout,"%d %d %d %d %d %d %d\n",week[6],week[0],week[1],week[2],week[3],week[4],week[5]);
     return 0;
 }
