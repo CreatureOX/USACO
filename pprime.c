3917d10a722b78e54541a192edf2deeaf82001f1
CreatureOX <785862780@qq.com>
2018-09-27 19:59:38 +0800

    Prime Palindromes

---
+++ pprime.c
@@ -0,0 +1,15 @@
+/*
+ID: keepagr1
+LANG: C
+PROG: pprime
+*/
+#include<stdio.h>
+#include<stdlib.h>
+int a,b;
+int main(){
+	FILE *fin = fopen("pprime.in","r");
+	FILE *fout = fopen("pprime.out","w");
+	fscanf(fin,"%d%d",&a,&b);
+	fprintf(fout,);
+	return 0;	
+}
